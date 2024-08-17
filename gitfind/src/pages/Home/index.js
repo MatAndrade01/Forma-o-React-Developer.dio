2554dfc318955843dee5d102e872020563760d44
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-08-17 08:33:47 -0300

    Finalizando o Button e Input da tela home(sem ser components)

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,9 +1,20 @@
+import Background from "../../assets/background.png"
 import { Header } from "../../components/Header";
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+      <img src={Background} className="background" alt="Background app"/>
+      <div className="info">
+        <div>
+          <input name="usuario" placeholder="@Username"/>
+          <button>Buscar</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
