26303f921ad9e05213bd71617a3d6ce2a9faa4ee
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-08-17 07:43:33 -0300

    Finalizando de montar as pastas!!

--- gitfind/src/index.js
+++ gitfind/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/Home';
+import App from './src/pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
